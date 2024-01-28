582b56ed28e36781fc3ccdfceb3798b31ea842de
Jozef Repáň <jozef.repan@nike.sk>
2024-01-28 11:12:38 +0000

    init
    - vite + eslint + react with typescript
    - prettier
    - more strict rules
    - basic todo

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': 'error',
+  },
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# SPA Template
+
+## TODO:
+- Multi language
+- REDUX
+- REACT QUERY
+
+
+## Available Scripts
+
+```bash
+$ npm run dev
+```
+
+
+
+```bash
+$ npm run build
+```
+
+```bash
+$ npm run preview
+```
+
+```bash
+$ npm run lint
+```
+
+## Tech stack
+- Vite + React TS
+- Eslint
+- Prettier
